b03798ac24eb15e62e766e18b664b072d67b413f
katothang <katothang@gmail.com>
2023-06-13 16:46:27 +0700

    update code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,61 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  WebComponentWrapper, WebComponentWrapperOptions
+} from '@angular-architects/module-federation-tools';
+const routes: Routes = [
+  {
+    path: "App",
+    loadChildren: () =>loadRemoteModule({
+    type:"module",
+    remoteEntry:"http://localhost:8080/remoteEntry.js",
+     
+    exposedModule:"App"
+    })
+    .then(m => {return m.AppModule})
+    },
+    {
+      path: "react-test",
+      loadChildren: () =>loadRemoteModule({
+      type:"module",
+      remoteEntry:"http://localhost:8080/remoteEntry.js",
+       
+      exposedModule:"demo"
+      })
+      .then(m => {return m.AppModule})
+      },
 
-const routes: Routes = [];
+    {
+      path: 'react',
+      component: WebComponentWrapper,
+      data: {
+        type: 'script',
+        remoteEntry: 'http://localhost:8080/remoteEntry.js',
+        remoteName: 'react',
+        exposedModule: './',
+        elementName: 'react-element',
+      } as WebComponentWrapperOptions,
+    },
+    {
+      path: 'react1',
+      component: WebComponentWrapper,
+      data: {
+        type: 'script',
+        remoteEntry: 'http://localhost:4204/remoteEntry.js',
+        remoteName: 'react',
+        exposedModule: './web-components',
+        elementName: 'react-element',
+      } as WebComponentWrapperOptions,
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+function then(arg0: (m: any) => any) {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  remoteComponent: any; 
+  ngOnInit(): void {
+ 
+  }
   title = 'host_angular';
+
+
 }
